81e241332793e151dbc460ebfc18bab2432fd799
Mohamed7-hub <mohamedtheone7@gmail.com>
2024-09-01 13:25:52 +0300

    Breakpoints-StackTrace-for-Debugging

---
+++ src/StackTraceExample.java
@@ -0,0 +1,12 @@
+public class StackTraceExample {
+
+    public static void main(String[] args) {
+        String str = null; // Intentional error: str is null
+        printLength(str);  // This will cause a NullPointerException
+    }
+
+    public static void printLength(String str) {
+        // This line will throw a NullPointerException since str is null
+        System.out.println("Length of the string: " + str.length());
+    }
+}
