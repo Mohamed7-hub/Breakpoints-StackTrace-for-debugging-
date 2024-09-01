31cf73866c0fa1559ab7eb7151325b8fccab271a
Mohamed7-hub <mohamedtheone7@gmail.com>
2024-09-01 14:12:02 +0300

    First-Example FactorialExample.java

--- src/FactorialExample.java
+++ src/FactorialExample.java
@@ -1,8 +1,8 @@
 public class FactorialExample {
 
     public static void main(String[] args) {
-        int number = 5; // You can set a breakpoint here to inspect 'number'
-        int result = calculateFactorial(number); // You can also set a breakpoint here to inspect 'result'
+        int number = 5; //seting a breakpoint here to inspect 'number'
+        int result = calculateFactorial(number); //also seting a breakpoint here to inspect 'result'
         System.out.println("Factorial of " + number + " is: " + result);
     }
 
@@ -12,7 +12,7 @@ public class FactorialExample {
         } else {
             int factorial = 1;
             for (int i = 1; i <= n; i++) {
-                factorial *= i; // Set a breakpoint here to inspect 'factorial' during each iteration
+                factorial *= i; // Seting a breakpoint here to inspect 'factorial' during each iteration
             }
             return factorial;
         }
